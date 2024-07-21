2f4b1ef13654b84ed18eb2e33d88ac09f0cd82ff
Aung Kaung Myint <aungkggmyint@gmail.com>
2024-07-21 23:03:30 +0700

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import Experience from "./components/Experience";
+import Configurator from "./components/Configurator";
+import { CustomizationProvider } from "./contexts/Customization";
 
-const App = () => {
+function App() {
   return (
-    <div className='text-red-400'>
-      Hello
-    </div>
-  )
+    <CustomizationProvider>
+      <div className="App">
+        <Canvas dpr={[1, 2]}>
+          <color attach="background" args={["#213547"]} />
+          <fog attach="fog" args={["#213547", 10, 20]} />
+          <Experience />
+        </Canvas>
+        <Configurator />
+      </div>
+    </CustomizationProvider>
+  );
 }
 
-export default App
+export default App;
